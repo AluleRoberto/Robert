af830077a56cbfeb36ec1f84e6bbc55fd84db462
AluleRoberto <alule.robert@hotmail.com>
2022-10-13 18:06:40 +0300

    first commit

---
+++ roberto.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	std::cout << "Hello World! Greetings from Roberto coding on Linux is too Cool!" << '\n'; 
+    std::cout << "C++ Rocks!" <<'\n';
+	return 0;
+}
