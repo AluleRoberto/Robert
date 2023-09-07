9654f6d1d6cec4c314c29edcfe56b34e6104b29b
AluleRoberto <1900800035@std.kyu.ac.ug>
2023-09-07 12:51:16 -0500

    Remove files

--- roberto.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char const *argv[])
-{
-	/* code */
-	std::cout << "Hello World! Greetings from Roberto coding on Linux is too Cool!" << '\n'; 
-    std::cout << "C++ Rocks!" <<'\n';
-	return 0;
-}
